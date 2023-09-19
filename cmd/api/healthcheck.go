15f1fc5ea37f1db68334a3f0cb6e35f20f06717a
skapskap <skaprange@gmail.com>
2023-09-19 10:59:02 -0300

    first commit

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "O servidor encontrou um problema e não conseguiu processar sua requisição"+
+			"", http.StatusInternalServerError)
+	}
+}
---
+++ cmd/api/routes.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/v1/healthcheck", app.healthcheckHandler)
+
+	return r
+}
